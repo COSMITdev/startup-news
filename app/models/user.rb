5b3d0dc73e88ba233dcda9525865103488a1901e
Sérgio Schnorr Júnior <jr.schnorr@gmail.com>
2013-10-06 15:19:36 -0300

    Allows underscore on username after one char

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          authentication_keys: [:login]
-  validates :username, presence: true, uniqueness: { case_sensitive: false }, format: { with: /\A[A-Za-z\d_]+\z/ }
+  validates :username, presence: true, uniqueness: { case_sensitive: false }, format: { with: /\A[A-Za-z\d]+[A-Za-z\d_]*\z/ }
   attr_accessor :login
 
   def self.find_first_by_auth_conditions(warden_conditions)
