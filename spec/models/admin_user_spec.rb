f18ac43a70dd455f21265b2ff15e8f5fc23ef680
Sérgio Schnorr Júnior <jr.schnorr@gmail.com>
2013-10-06 16:25:10 -0300

    Some tests for AdminUser

--- spec/models/admin_user_spec.rb
+++ spec/models/admin_user_spec.rb
@@ -1,5 +1,8 @@
 require 'spec_helper'
 
 describe AdminUser do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Validations" do
+    it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:password) }
+  end
 end
