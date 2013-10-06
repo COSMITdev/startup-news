5643efc5e2d4ad17c15576c0525f2f606cb08c4e
Sérgio Schnorr Júnior <jr.schnorr@gmail.com>
2013-10-06 16:27:41 -0300

    User spec: validate uniqueness of username with case_insensitive

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,11 +2,11 @@ require 'spec_helper'
 
 describe User do
   describe "Validations" do
-    it { should validate_presence_of :username }
-    it { should validate_presence_of :email }
-    it { should validate_presence_of :password }
-    it { should validate_uniqueness_of :username }
-    it { should validate_uniqueness_of :email }
+    it { should validate_presence_of(:username) }
+    it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:password) }
+    it { should validate_uniqueness_of(:username).case_insensitive }
+    it { should validate_uniqueness_of(:email) }
 
     context "#username" do
       it { should_not allow_value('á').for(:username) }
