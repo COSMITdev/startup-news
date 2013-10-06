bf9d8716f47c3e1a860f7d2f3e970214724bd52e
Sérgio Schnorr Júnior <jr.schnorr@gmail.com>
2013-10-06 16:26:00 -0300

    Just reorder User model validation and attr_accessor

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,10 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          authentication_keys: [:login]
-  validates :username, presence: true, uniqueness: { case_sensitive: false }, format: { with: /\A[A-Za-z\d]+[A-Za-z\d_]*\z/ }
+
   attr_accessor :login
+
+  validates :username, presence: true, uniqueness: { case_sensitive: false }, format: { with: /\A[A-Za-z\d]+[A-Za-z\d_]*\z/ }
 
   def self.find_first_by_auth_conditions(warden_conditions)
     conditions = warden_conditions.dup
