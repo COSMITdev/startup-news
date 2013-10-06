716470df13e7ed52a703e7e97b4f6cfbc9c6f6a3
Sérgio Schnorr Júnior <jr.schnorr@gmail.com>
2013-10-06 16:03:12 -0300

    Permitted params and forms on admin/user

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,3 +1,17 @@
 ActiveAdmin.register User do
+  form do |f|
+    f.inputs do
+      f.input :username
+      f.input :email
+      f.input :password
+      f.input :password_confirmation
+    end
+    f.actions
+  end
 
+  controller do
+    def permitted_params
+      params.permit(user: [:username, :email, :password, :password_confirmation])
+    end
+  end
 end
