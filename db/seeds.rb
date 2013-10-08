d8571ed2ee28dae604f47663b61d4bdf335b702b
Patrick Müller <patrick.e.muller@gmail.com>
2013-10-08 17:05:55 -0300

    Inserted content on seeds for development

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+if Rails.env.development?
+  10.times do |i| 
+    User.create!([
+      username: "username_teste_#{i}",
+      email: "email#{i}@email.com",
+      password: "123123123"
+    ])
+  end
+
+  100.times do |i|
+    10.times do |j|
+      News.create!([
+        user: User.find(j+1),
+        title: "Notícia de teste #{i}#{j}",
+        link: 'http://lists.gnupg.org/pipermail/gnupg-announce/2013q4/000334.html',
+        text: 'Apenas um conteúdo de testes mesmo'
+      ])
+    end
+  end
+end
