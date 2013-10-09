587894f41325c2aa89816af4416050895f58ce70
Sérgio Schnorr Júnior <jr.schnorr@gmail.com>
2013-10-09 00:51:40 -0300

    Tests for application_controller#configure_permitted_parameters

---
+++ spec/controllers/application_controller_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+describe ApplicationController do
+  describe "#configure_permitted_parameters" do
+    before do
+      subject.stub(:resource_class).and_return(User)
+      subject.stub(:resource_name).and_return("User")
+      subject.__send__(:configure_permitted_parameters)
+    end
+
+    context "devise_parameter_sanitizer for sign_up" do
+      it { controller.devise_parameter_sanitizer.for(:sign_up).should include(:username) }
+      it { controller.devise_parameter_sanitizer.for(:sign_up).should include(:email) }
+    end
+
+    context "devise_parameter_sanitizer for :sign_in" do
+      it { controller.devise_parameter_sanitizer.for(:sign_in).should include(:username) }
+    end
+
+    context "devise_parameter_sanitizer for :account_update" do
+      it { controller.devise_parameter_sanitizer.for(:account_update).should include(:email) }
+      it { controller.devise_parameter_sanitizer.for(:account_update).should include(:username) }
+    end
+  end
+end
