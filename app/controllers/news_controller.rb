24ce0485e04bf8481242dae6d0a40cdd311043a8
Sérgio Schnorr Júnior <jr.schnorr@gmail.com>
2013-10-08 23:29:21 -0300

    Remove user_id from permitted_params (News)

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,9 +1,15 @@
 class NewsController < InheritedResources::Base
   before_filter :authenticate_user!, except:[:index]
 
+  def create
+    @news = News.new(permitted_params[:news])
+    @news.user = current_user
+    create!
+  end
+
   private
 
   def permitted_params
-    params.permit(news:[:user_id, :title, :link, :text])
+    params.permit(news: [:title, :link, :text])
   end
 end
