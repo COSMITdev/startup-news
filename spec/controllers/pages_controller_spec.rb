bbb3cd6808cb522e05e81de18a1a40ed75244579
Bruno Pazzim <bpazzim@gmail.com>
2013-11-06 11:43:06 -0200

    Added specs to mailer

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -6,12 +6,22 @@ describe PagesController do
       get :about
       expect(response).to render_template('about')
     end
+
+    it "GET contact" do
+      get :contact
+      expect(response).to render_template('contact')
+    end
   end
 
   context "When Logged Out" do
     it "GET about" do
       get :about
       expect(response).to render_template('about')
+    end
+
+    it "GET contact" do
+      get :contact
+      expect(response).to render_template('contact')
     end
   end
 end
--- spec/mailers/contact_spec.rb
+++ spec/mailers/contact_spec.rb
@@ -1,5 +1,22 @@
 require "spec_helper"
 
 describe Contact do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "send_email" do
+    let(:mail) { Contact.send_email({}) }
+
+    # ensure that the receiver is correct
+    it "renders the receiver email" do
+      mail.to.should include('contato@codeland.com.br')
+    end
+
+    # ensure that the sender is correct
+    it "renders the sender email" do
+      mail.from.should include("no-reply@codeland.com.br")
+    end
+
+    # ensure that the subject is correct
+    it "renders the subject" do
+      mail.subject.should == "Contato StartupNews"
+    end
+  end
 end
