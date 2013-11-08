f8ca0210a789f7b9e04dedb4f1789b7ae69e12f5
Sérgio Schnorr Júnior <jr.schnorr@gmail.com>
2013-11-08 12:33:26 -0200

    Start refactoring omniauth_callbacks_controller

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,25 +1,23 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
     @auth = request.env["omniauth.auth"]
-    @authentication = Authentication.where(provider: @auth.provider, uid: @auth.uid)
-    @user = User.where(email: @auth.info.email)
+    @authentication = Authentication.where(provider: @auth.provider, uid: @auth.uid).first
+    @user = User.where(email: @auth.info.email).first
 
-    if @authentication.any?
-      if @authentication.first.user.persisted?
-        set_flash_message(:notice, :success, kind: "Facebook") if is_flashing_format?
-        sign_in_and_redirect @authentication.first.user, event: :authentication
-      end
+    if @authentication
+      set_flash_message(:notice, :success, kind: "Facebook") if is_flashing_format?
+      sign_in_and_redirect @authentication.user, event: :authentication
     else
-      if @user.any?
-        @authenticated_user = @user.first.create_authentication(provider: @auth.provider, uid: @auth.uid)
+      if @user
+        @authenticated_user = @user.create_authentication(provider: @auth.provider, uid: @auth.uid)
         set_flash_message(:notice, :success, kind: "Facebook") if is_flashing_format?
         sign_in_and_redirect @authenticated_user.user, event: :authentication
       else
         password = Devise.friendly_token[0,20]
-        user = User.create!(username:@auth.info.nickname.gsub('.', ''),
-                           email:@auth.info.email,
-                           password:password,
-                           password_confirmation:password
+        user = User.create!(username: @auth.info.nickname.gsub('.', ''),
+                           email: @auth.info.email,
+                           password: password,
+                           password_confirmation: password
                           )
         set_flash_message(:notice, :success, kind: "Facebook") if is_flashing_format?
         sign_in_and_redirect user, event: :authentication
