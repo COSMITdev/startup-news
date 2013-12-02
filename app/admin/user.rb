38e9bd94cb5cbe48b824bd6a794cc2794a198828
Sérgio Schnorr Júnior <jr.schnorr@gmail.com>
2013-12-01 23:31:13 -0200

    Change per_page on export User in ActiveAdmin when is .csv

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -13,5 +13,10 @@ ActiveAdmin.register User do
     def permitted_params
       params.permit(user: [:username, :email, :password, :password_confirmation])
     end
+
+    def per_page
+      return User.count + 100 if request.format == 'text/csv'
+      super
+    end
   end
 end
